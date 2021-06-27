b57e2292e044a8fc685a1689a3219fbc623b8d0b
Yauheni Cheuzhyk <eugene.chevzh@gmail.com>
2021-06-27 18:23:40 +0300

    Task 1 - Utils class fix

--- Module 5 - Profiling/Task1/Task1/Program.cs
+++ Module 5 - Profiling/Task1/Task1/Program.cs
@@ -20,7 +20,19 @@ namespace Task1
 
             string password = "testPassword123testPassword123testPassword123testPassword123testPassword123";
 
-            GetTime(GeneratePasswordHashUsingSalt, "GeneratePasswordHashUsingSalt", password, salt);
+            //GetTime(GeneratePasswordHashUsingSalt, "GeneratePasswordHashUsingSalt", password, salt);
+
+            var iterate = 10000;
+            var pbkdf2 = new CustomRfc2898DeriveBytes(password, salt, iterate);
+
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            var passwordHash = Convert.ToBase64String(hashBytes);
 
         }
 
@@ -28,7 +40,7 @@ namespace Task1
         {
 
             var iterate = 10000; 
-            var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate); 
+            var pbkdf2 = new CustomRfc2898DeriveBytes(passwordText, salt, iterate); 
 
             byte[] hash = pbkdf2.GetBytes(20);
 
