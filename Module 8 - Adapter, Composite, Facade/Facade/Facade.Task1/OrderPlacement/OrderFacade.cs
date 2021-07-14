4c6f5a7e9047398487d5e3279bbb93eb7cde6269
Yauheni Cheuzhyk <eugene.chevzh@gmail.com>
2021-07-14 18:22:23 +0300

    Module 8 - init

---
+++ Module 8 - Adapter, Composite, Facade/Facade/Facade.Task1/OrderPlacement/OrderFacade.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Facade.Task1.OrderPlacement
+{
+    public class OrderFacade
+    {
+        public void PlaceOrder(string productId, int quantity, string email)
+        {
+            throw new Exception();
+        }
+    }
+}
