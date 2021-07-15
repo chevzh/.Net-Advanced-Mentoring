be80478bbe8cfbf13195d009d56b2eac605c80fe
Yauheni Cheuzhyk <eugene.chevzh@gmail.com>
2021-07-15 18:04:14 +0300

    Adapter - completed

---
+++ Module 8 - Adapter, Composite, Facade/Adapter/Adapter.Task1/MyPrinterAdapter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adapter.Task1
+{
+    public class MyPrinterAdapter: IMyPrinter
+    {
+        private readonly Printer printer;
+
+        public MyPrinterAdapter(Printer printer)
+        {
+            this.printer = printer;
+        }
+
+        public void Print<T>(IElements<T> elements)
+        {
+            printer.Print<T>(new MyContainer<T>(elements));
+        }
+    }
+}
