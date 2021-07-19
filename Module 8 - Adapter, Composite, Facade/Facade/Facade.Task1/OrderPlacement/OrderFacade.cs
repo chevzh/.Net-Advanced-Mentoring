690a1535bdde4eea70d22645636a2f9d19370eed
Yauheni Cheuzhyk <eugene.chevzh@gmail.com>
2021-07-19 12:39:23 +0300

    Facade - complete

--- Module 8 - Adapter, Composite, Facade/Facade/Facade.Task1/OrderPlacement/OrderFacade.cs
+++ Module 8 - Adapter, Composite, Facade/Facade/Facade.Task1/OrderPlacement/OrderFacade.cs
@@ -4,9 +4,33 @@ namespace Facade.Task1.OrderPlacement
 {
     public class OrderFacade
     {
+        InvoiceSystem invoiceSystem;
+        PaymentSystem paymentSystem;
+        ProductCatalog productCatalog;
+
+        public OrderFacade(InvoiceSystem invoiceSystem, PaymentSystem paymentSystem, ProductCatalog productCatalog)
+        {
+            this.invoiceSystem = invoiceSystem;
+            this.paymentSystem = paymentSystem;
+            this.productCatalog = productCatalog;
+        }
+
         public void PlaceOrder(string productId, int quantity, string email)
         {
-            throw new Exception();
+            Product product = productCatalog.GetProductDetails(productId);
+
+            Payment payment = new Payment()
+            {
+                ProductId = product.Id,
+                ProductName = product.Name,
+                Quantity = quantity,
+                TotalPrice = product.Price
+            };
+
+            Invoice invoice = new Invoice() { ProductId = product.Id, CustomerEmail = email, ProductName = product.Name, Quantity = quantity, TotalPrice = payment.TotalPrice };
+
+            paymentSystem.MakePayment(payment);
+            invoiceSystem.SendInvoice(invoice);
         }
     }
 }
